cdcd8f894d655c3a3a563ec78f3b6b2f031e55f3
RoNRiShaV <rishavecr2897@gmail.com>
2025-10-24 02:39:26 +0530

    y

--- Backend/database.py
+++ Backend/database.py
@@ -1,11 +1,12 @@
 import mysql.connector
 from mysql.connector import pooling
+import os
 
 dbconfig = {
-    "host": "localhost",
-    "user": "csbs_19",
-    "password": "cristiano07",
-    "database": "event_registration"
+    "host": os.getenv("DB_HOST", "db"),      # use the Docker service name
+    "user": os.getenv("DB_USER", "csbs_19"),
+    "password": os.getenv("DB_PASSWORD", "cristiano07"),
+    "database": os.getenv("DB_NAME", "event_registration")
 }
 
 connection_pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **dbconfig)
